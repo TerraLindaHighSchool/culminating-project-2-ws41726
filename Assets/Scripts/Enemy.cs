bb14d5f6d7ee20568a866623b24ca8c0e3531f58
ws41726 <ws41726@TL-HUB1-011100>
2021-01-05 15:24:44 -0800

    particles added
    particles are added, but need to be fixed a little so they don't all play at once. Started working on title Screen

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public ParticleSystem explosionParticle;
+
+    private Rigidbody enemyRb;
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        
+        enemyRb.AddForce(lookDirection * speed);
+        //Debug.Log("Update of Enemy" + transform.position.y);
+        if (transform.position.y < -3)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    private GameManager gameManager;
+    private float powerupStrength = 15.0f;
+    public float speed = 5.0f;
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public ParticleSystem explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        if (transform.position.y < -3)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.GameOver();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+          if (other.CompareTag("Powerup"))
+            {
+                hasPowerup = true;
+                Destroy(other.gameObject);
+                StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+            }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+            {
+                Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+                Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+                Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+                Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+                enemyRigidBody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+    }
+}
